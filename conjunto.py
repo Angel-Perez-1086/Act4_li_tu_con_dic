da87002eb79f35e00da8db963613a1c12eba79ec
Angel Perez <164534035+Angel-Perez-1086@users.noreply.github.com>
2024-09-06 12:20:37 -0600

    Add files via upload

---
+++ conjunto.py
@@ -0,0 +1,8 @@
+misamores = {"jett", "clove", "neon"}
+print(misamores)
+
+print("cantidad de amores")
+print(len(misamores))
+print("----Mis amores---")
+for x in misamores:
+  print(x)
---
+++ diccionario.py
@@ -0,0 +1,16 @@
+
+estediccionario = {
+  "id": "123",
+  "Nombre": "Esteban",
+  "apellido": "Oropeza",
+  "edad": "17",
+  "email": "Carlitosuwu123@gmail.com"
+}
+print(estediccionario)
+
+
+for x in estediccionario :
+    print(estediccionario[x])
+
+print("---email---")
+print(estediccionario["email"])
